054831049f0891c8944e4c82082a6e1a6341b5e1
VISHNU VIJAYAN <vijayanvishnu2406@gmail.com>
2024-05-04 10:16:17 +0530

    Dijkstras Algorithm

--- Dijkstras.java
+++ Dijkstras.java
@@ -1,4 +1,4 @@
-import java.util.;
+import java.util.*;
 class Pair{
     int node;
     int cost;
@@ -9,36 +9,36 @@ class Pair{
 }
 public class Solution {
     public int helper(int[][] edges, int n, int start) {
-        ListPair graph[] = new ArrayList[n + 1];
+        List<Pair> graph[] = new ArrayList[n + 1];
         int cost[] = new int[n + 1];
-        Arrays.fill(cost,Integer.MAX_VALUE2);
+        Arrays.fill(cost,Integer.MAX_VALUE/2);
         cost[start] = cost[0] = 0;
-        for(int i = 0 ; i = n ; i ++){
-            graph[i] = new ArrayList();
+        for(int i = 0 ; i <= n ; i ++){
+            graph[i] = new ArrayList<>();
         }
-        for(int i = 0; i  edges.length ; i++){
+        for(int i = 0; i < edges.length ; i++){
             int srt = edges[i][0];
             int dst = edges[i][1];
             int cst = edges[i][2];
             graph[srt].add(new Pair(dst,cst));
         }
         
-        // dijkstra's algorithm
-        PriorityQueuePair pq = new PriorityQueue((a,b)-Integer.compare(a.cost,b.cost));
+        //dijkstra's algorithm
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->Integer.compare(a.cost,b.cost));
         pq.add(new Pair(start,0));
         int ans = 0;
-        while(pq.size()  0){
+        while(pq.size() > 0){
             Pair poll = pq.poll();
-            for(Pair i  graph[poll.node]){
-                if(cost[i.node]  poll.cost + i.cost){
+            for(Pair i : graph[poll.node]){
+                if(cost[i.node] > poll.cost + i.cost){
                     cost[i.node] = poll.cost + i.cost;
                     pq.add(new Pair(i.node,cost[i.node]));
                 }
             }
         }
-        for(int i  cost){
+        for(int i : cost){
             ans = Math.max(ans,i);
         }
-        return ans == Integer.MAX_VALUE  2  -1  ans;
+        return ans == Integer.MAX_VALUE / 2 ? -1 : ans;
     }
 }
